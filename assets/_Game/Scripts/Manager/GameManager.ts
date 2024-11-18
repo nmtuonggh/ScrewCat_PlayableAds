5863108b4954cff6eccd7e8d996865a117b4fa94
nmtuonggh <tuongwork@gmail.com>
2024-11-18 15:55:24 +0700

    start test booster

--- assets/_Game/Scripts/Manager/GameManager.ts
+++ assets/_Game/Scripts/Manager/GameManager.ts
@@ -32,9 +32,6 @@ export class GameManager extends Component
     @property( MultiScreneController )
     private multiScreenController: MultiScreneController = null;
 
-    @property( Node )
-    public loseUI: Node = null;
-
     @property( ScrewData )
     public screwData: ScrewData = null;
 
